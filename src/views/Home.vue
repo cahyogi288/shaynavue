c344f6d6c6fdfba01e6cf1ebce435ce48a3b0162
cahyogi <cahyogi288@gmail.com>
2020-08-10 15:19:04 +0700

    add projek shayna

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <header-shayna />
+    <hero-shayna />
+    <woman-shayna />
+    <insta-shayna />
+    <logo-shayna />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomanShayna from '@/components/WomanShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import LogoShayna from '@/components/Logo.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomanShayna,
+    InstaShayna,
+    LogoShayna,
+    Footer
+  }
+}
+</script>
